f280ae5fa03bf94435fbb0658235a9cae7a11964
Deepthi-Vunnam <143685045+Deepthi-Vunnam@users.noreply.github.com>
2026-05-24 16:39:16 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { Provider } from 'react-redux'
-import store from './Redux/store';
+import store from './Redux/Store.jsx';
 
 
 createRoot(document.getElementById('root')).render(
