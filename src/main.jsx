96093dff29e4ffd60c8255d230520c271d9ab03c
Deepthi-Vunnam <143685045+Deepthi-Vunnam@users.noreply.github.com>
2026-05-24 16:54:34 +0530

    Changed BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { Provider } from 'react-redux'
@@ -11,8 +11,8 @@ import store from './Redux/Store.jsx';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+</HashRouter>
   </Provider>
 )
